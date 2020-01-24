8a2b9e5f0142333648be3b6033b43c4fedf5ca72
Hazel Wright <hazel.wright@live.co.uk>
2020-01-24 20:26:33 +0000

    surprising thing about randomness in Go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+    "math/rand"
+)
+
+func get_random_number(min int, max int) int {
+    // We need to seed the random number generator with the current datetime
+    // as without it Go uses a shared Source(???) to generate the same
+    // deterministic sequence of values every time this program is run.
+
+    rand.Seed(time.Now().UnixNano())
+    return rand.Intn(max - min + 1) + min
+}
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Println("hello, world\n")
+    fmt.Println("The time is currently ", time.Now())
+    fmt.Println("Here is a random number between 3 and 10: ", get_random_number(3, 10))
 }
